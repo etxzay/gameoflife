6a21163f36c21d8f9d71ac7144e188c78d27082b
etxzay <pasha.zhukov@gmail.com>
2015-12-23 19:14:31 +0300

    add clear universe feature new universe now appears with dead particles instead of nil add draw method to universe

--- lib/gameoflife/universe.rb
+++ lib/gameoflife/universe.rb
@@ -1,9 +1,11 @@
+require "gameoflife/particle"
+
 class Universe
 
   def initialize(width, height)
     @width = width
     @height = height
-    @particles = Array.new(width) { Array.new(height) }
+    clear
   end
 
   def neighbours(particle)
@@ -11,19 +13,35 @@ class Universe
   end 
 
   def add(particle)
-    @particles[particle.x][particle.y] = particle
+    @particles[particle.y][particle.x] = particle
   end
 
   def get(x,y)
-    @particles[x][y]
+    @particles[y][x]
+  end
+
+  def clear
+    @particles = Array.new(@height) { Array.new(@width) }
+    for y in 0..@height-1 do
+      for x in 0..@width-1 do
+        add(Particle.new(x, y, false))
+      end
+    end
   end
 
   def generate
 
   end
 
-  def print
-
+  def draw
+    @particles.each { |row|
+      print "\n"
+      row.each { |particle|
+        if particle != nil
+          particle.draw
+        end
+      }
+    }
   end
 
 end
--- spec/universe_spec.rb
+++ spec/universe_spec.rb
@@ -2,20 +2,27 @@ require 'gameoflife/universe'
 
 describe Universe do
 
-  let(:universe) { Universe.new(4,4) }
+  let(:universe) { Universe.new(4, 4) }
+  let(:particle) { Particle.new(2, 2, true) }
 
-  describe 'new universe' do
-    it 'should contain empty two-dimensional array as particle storage' do
-      expect(universe.get(3,3)).to eq nil
-    end
+  it 'new universe contain two-dimensional array as particle storage' do
+    expect(universe.get(3,3)).not_to eq nil
   end
 
-  describe 'add & get particles methods' do
-    it 'should add a particle to universe' do
-      particle = Particle.new(2,2,true)
-      universe.add(particle)
-      expect(universe.get(2,2)).to eq particle
-    end
+  it 'add a particle to universe' do
+    universe.add(particle)
+    expect(universe.get(2, 2)).to eq particle
+  end
+
+  it 'clear should empty particles array' do
+    universe.add(particle)
+    universe.clear
+    expect(universe.get(2, 2).alive?).to eq false
+  end
+
+  it 'draw all containing particles' do
+    universe.add(particle)
+    expect{ universe.draw }.to output("\n....\n....\n..X.\n....").to_stdout
   end
 
 end
