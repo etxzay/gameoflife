c09a99a438f4717b18fb1a1ed5fa182784c275cd
etxzay <pasha.zhukov@gmail.com>
2015-12-23 14:41:03 +0300

    remove redundant universer creation

--- spec/universe_spec.rb
+++ spec/universe_spec.rb
@@ -13,7 +13,6 @@ describe Universe do
   describe 'add & get particles methods' do
     it 'should add a particle to universe' do
       particle = Particle.new(2,2,true)
-      universe = Universe.new(4,4)
       universe.add(particle)
       expect(universe.get(2,2)).to eq particle
     end
