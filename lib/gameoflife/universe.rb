983da40b7bd8056225c9b88498f26961cacd606f
etxzay <pasha.zhukov@gmail.com>
2015-12-24 13:22:39 +0300

    clear now kill existing particles instead of creating new

--- lib/gameoflife/universe.rb
+++ lib/gameoflife/universe.rb
@@ -23,12 +23,27 @@ class Universe
   end
 
   def clear
-    @particles = Array.new(@height) { Array.new(@width) }
+  
+    if @particles == nil
+      @particles = Array.new(@height) { Array.new(@width) }
+    end
+
     for y in 0..@height-1 do
       for x in 0..@width-1 do
-        add(Particle.new(x, y, false))
+
+        current = get(x,y)
+
+        if current == nil
+          current = Particle.new(x, y, false)
+        else
+          current.alive = false
+        end
+
+        add(current)
+
       end
     end
+
   end
 
   def generate
