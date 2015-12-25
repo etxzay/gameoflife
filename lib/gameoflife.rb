6caeab7d884aa6516a0a151eb21625af758c93c1
etxzay <pasha.zhukov@gmail.com>
2015-12-25 15:40:59 +0300

    reading initial state, creating universe

--- lib/gameoflife.rb
+++ lib/gameoflife.rb
@@ -1,5 +1,37 @@
 require "gameoflife/version"
+require "gameoflife/universe"
+require "gameoflife/telescope"
+require "gameoflife/csvparser"
 
 module Gameoflife
-  # Your code goes here...
+
+  def init(filename)
+
+    parser = CSVParser.new(filename)
+    data = parser.parse
+
+    coordinates = data.pop
+
+    @universe = Universe.new(coordinates[0].to_i, coordinates[1].to_i)
+    data.each { |pair|
+      @universe.animate(pair[0].to_i, pair[1].to_i)
+    }
+
+  end
+
+  def turn
+  end
+
+  def end?
+  end
+
+  def draw
+  end
+
+  def play
+  end
+
+  def start
+  end
+
 end
--- spec/gameoflife_spec.rb
+++ spec/gameoflife_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 describe Gameoflife do
+
+  let(:dummy_class) { Class.new { include Gameoflife }.new }
+  let(:coordinates) { [["4","4"],["2","1"],["1","2"]] }
+
+  before do
+    allow_any_instance_of(CSVParser).to receive(:parse).and_return(coordinates)
+  end
+
   it 'has a version number' do
     expect(Gameoflife::VERSION).not_to be nil
   end
 
-#  it 'does something useful' do
-#    expect(false).to eq(true)
-#  end
+  it 'able to read initial state' do
+    dummy_class.init("test.txt")
+    expect(dummy_class.instance_variable_get(:@universe).instance_of?(Universe)).to eq true
+  end
+
+  it 'creates new generation' do
+  end
+
+  it 'displays state' do
+  end
+
+  it 'checks end of game condition' do
+  end
+
+  it 'makes turns untill end of game' do
+  end
+
 end
